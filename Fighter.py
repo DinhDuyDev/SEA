aa580cd6d1aca81f28c50b2ed849ef850d393536
Sandy <dinhduygiangvo@gmail.com>
2025-09-23 22:41:19 -0400

    Added comments to warn about the tangledness of the code

--- Fighter.py
+++ Fighter.py
@@ -137,6 +137,7 @@ class Fighter:
         self.x = min(max(16, self.x), WINDOW_WIDTH-16)
         self.y = min(max(16, self.y), WINDOW_HEIGHT-16)
 
+    # Arguments of the add fighter function is becoming bloated.
     def __init__(self, x, y, name=f"NONAME{fighter_id}", hp=full_health):
         self.x = x
         self.y = y
