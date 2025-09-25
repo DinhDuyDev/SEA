84c66657d0e8a43722c289d631bb9f2734ff914f
Sandy <dinhduygiangvo@gmail.com>
2025-09-24 20:20:25 -0400

    Refactored this whole damned code base

--- game_setup.py
+++ game_setup.py
@@ -163,7 +163,7 @@ def get_keys(event):
     elif event.key == pygame.K_BACKSPACE:
         TextBox.curr_name_string = TextBox.curr_name_string[:-1]
     elif event.key == pygame.K_g:
-        if len(NameQueue.names_queue) == 0:
+        if len(NameQueue.names_queue) != 0:
             NameQueue.names_queue.pop()
     else:
         TextBox.curr_name_string += event.unicode
