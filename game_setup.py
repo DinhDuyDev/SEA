d9327cd568f3928c1fed1ddee325a5c3cc6cd003
Sandy <dinhduygiangvo@gmail.com>
2025-09-25 19:55:38 -0400

    Moved get_keys as a helper function

--- game_setup.py
+++ game_setup.py
@@ -15,6 +15,23 @@ my_font = pygame.font.SysFont('./Kranky/Kranky-Regular.ttf', 15)
 # Defining functions
 
 def adding_names():
+
+    def get_keys(e):
+        if e.key == pygame.K_RETURN:
+            NameQueue.names_queue.append(TextBox.curr_name_string)
+            TextBox.curr_name_string = ""
+        elif e.key == pygame.K_TAB:
+            Game.giving_names = False  # Move into play mode!
+            Game.curr_state = "FIGHTING"
+        elif e.key == pygame.K_BACKSPACE:
+            TextBox.curr_name_string = TextBox.curr_name_string[:-1]
+        elif e.key == pygame.K_LSHIFT or e.key == pygame.K_RSHIFT:
+            if len(NameQueue.names_queue) != 0:
+                NameQueue.names_queue.pop()
+        else:
+            if e.key != pygame.K_ESCAPE:  # and Game.giving_names:
+                TextBox.curr_name_string += event.unicode
+
     for event in pg.event.get():
         if event.type == pygame.QUIT:
             Game.running = False
