0a9c8399bcda8a61a71a68a8eb3a2752b35ff3be
Sandy <dinhduygiangvo@gmail.com>
2025-09-26 16:45:04 -0400

    Added new files

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ def main():
     clock = pygame.time.Clock()
     Game.running = True
 
+    Button(WINDOW_WIDTH/2, WINDOW_HEIGHT/2+32, 128, 32, "START GAME", "MENU")
+    Button(WINDOW_WIDTH/2, WINDOW_HEIGHT/2+64, 128, 32, "EXIT GAME", "MENU")
+    Button(WINDOW_WIDTH/2, WINDOW_HEIGHT/2+90, 128, 32, "Add Name", "ADDING_NAMES")
+    Button(WINDOW_WIDTH/2, WINDOW_HEIGHT/2+122, 128, 32, "Delete Name", "ADDING_NAMES")
+    Button(WINDOW_WIDTH/2, WINDOW_HEIGHT/2+154, 128, 32, "Proceed to fight!", "ADDING_NAMES")
+
+    Text(WINDOW_WIDTH/2, WINDOW_HEIGHT/2 - 128, "AI SUPER SHOWDOWN", "MENU")
+    Text(WINDOW_WIDTH/2, WINDOW_HEIGHT/2 - 128, "ENTER THE NAMES", "ADDING_NAMES")
+
     while Game.running:
         # for event in pg.event.get():
         #     if event.type == pygame.QUIT:
