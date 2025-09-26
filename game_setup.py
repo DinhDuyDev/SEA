b6ad789284821854d5a2c6e08baab1efb8ad0875
Sandy <dinhduygiangvo@gmail.com>
2025-09-26 18:51:38 -0400

    Added condition checking

--- game_setup.py
+++ game_setup.py
@@ -150,8 +150,11 @@ def adding_names():
                 if len(NameQueue.names_queue) != 0:
                     NameQueue.names_queue.pop()
             elif proceed_button.button_rect.collidepoint(mx, my):
-                Game.giving_names = False  # Move into play mode!
-                Game.curr_state = "FIGHTING"
+                if len(NameQueue.names_queue) > 1:
+                    Game.giving_names = False  # Move into play mode!
+                    Game.curr_state = "FIGHTING"
+                else:
+                    OnDemandText(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, "Minimum: 2 names.")
 
     names_surf = my_font.render(f"> {TextBox.curr_name_string}|", False, (255, 0, 0))
     names_rect = names_surf.get_rect(topleft=(64, 80))
