d2d240cd1fda7426ef00c74da1aa801f06b547ca
Sandy <dinhduygiangvo@gmail.com>
2025-09-26 18:58:32 -0400

    Increased

--- game_setup.py
+++ game_setup.py
@@ -165,7 +165,7 @@ def adding_names():
 
     for i, v in enumerate(NameQueue.names_queue):
         names_surf_list = my_font.render(f"{i + 1} : {v}", False, (255, 0, 0))
-        names_rect_list = names_surf.get_rect(topleft=(WINDOW_WIDTH / 2 + 128, i * 16 + 80))
+        names_rect_list = names_surf.get_rect(topleft=(WINDOW_WIDTH / 2 + 128, i * 16 + 64))
         Game.draw_dest.blit(names_surf_list, names_rect_list)
 
 
