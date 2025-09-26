0a12fce6916095f5cbee869a3790808cf5c89221
Sandy <dinhduygiangvo@gmail.com>
2025-09-26 18:54:49 -0400

    Maxed at 10

--- game_setup.py
+++ game_setup.py
@@ -142,7 +142,7 @@ def adding_names():
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mx, my = mouse_coords()[0], mouse_coords()[1]
             if add_name_button.button_rect.collidepoint(mx, my):
-                if len(NameQueue.names_queue) < 11:
+                if len(NameQueue.names_queue) < 10:
                     NameQueue.names_queue.append(TextBox.curr_name_string)
                     TextBox.curr_name_string = ""
                 else:
